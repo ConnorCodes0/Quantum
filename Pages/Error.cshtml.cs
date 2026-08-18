7c3e3e5a17357493583135ccb8c57018c7e7d6c9
Connor <b036050n@student.staffs.ac.uk>
2026-08-18 23:00:59 +0100

    Renamed project to Quantum

--- Pages/Error.cshtml.cs
+++ Pages/Error.cshtml.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Diagnostics;
 
-namespace TextEditorSyntaxHighlighter.Pages
+namespace Quantum.Pages
 {
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     [IgnoreAntiforgeryToken]
