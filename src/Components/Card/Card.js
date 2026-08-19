b616cb7db1b1effb58f5d3908f4fc1655ebaf8b1
Connor <b036050n@student.staffs.ac.uk>
2026-08-19 12:57:51 +0100

    Added css and js bundling and minification

---
+++ src/Components/Card/Card.js
@@ -0,0 +1,13 @@
+﻿const Card = (() => {
+    'use strict';
+
+    function init() {
+        console.log("Card component loaded");
+    }
+
+    return {
+        init
+    };
+})();
+
+export default Card;
---
+++ src/js/Main.js
@@ -0,0 +1,7 @@
+﻿import Card from "../Components/Card/Card.js";
+
+const components = [
+    Card
+];
+
+components.forEach(component => component.init());
